cac480b41657399be124d2cd2235c1bb2aef4532
elicarcar <elisabetta.carcar@gmail.com>
2020-04-17 13:59:47 +0200

    Alert reducer is complete

--- client/src/reducers/alert.js
+++ client/src/reducers/alert.js
@@ -1,5 +1,6 @@
-const initialState = [];
 import { SET_ALERT, REMOVE_ALERT } from "../actions/types";
+
+const initialState = [];
 
 export default function (state = initialState, action) {
   const { type, payload } = action;
@@ -8,5 +9,7 @@ export default function (state = initialState, action) {
       return [...state, payload];
     case REMOVE_ALERT:
       return state.filter((alert) => alert.id !== payload);
+    default:
+      return state;
   }
 }
